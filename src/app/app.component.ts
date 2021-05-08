7b705a8cc13fa11abfcf0c72e5e003b3bff5193d
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2021-05-08 13:49:37 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Task } from './task/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'kanban-fire';
+  todo: Task[] = [
+    { title: 'Buy mild', description: 'Go to the store to get milk' },
+    { title: 'Create Kanban board', description: 'Develop a Kanban app' },
+  ];
 }
